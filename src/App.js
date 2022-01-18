616b7700e61fee354f82727b54973293ddfa9688
Akshay <AkshayAwate>
2022-01-18 17:28:23 +0530

    fix: auto-approve tf

--- src/App.js
+++ src/App.js
@@ -3,21 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+    <img src="https://images.app.goo.gl/QRsor7vbYLH4pcjp9" alt="W3Schools.com"> </img>
+    
     </div>
   );
 }
