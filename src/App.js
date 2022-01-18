1d567e6817004d152a6d0fbd72f819b6c989f0e9
Akshay <AkshayAwate>
2022-01-18 17:40:07 +0530

    fix: auto-approve tf

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    
-    <div>
-    <img src="https://images.app.goo.gl/QRsor7vbYLH4pcjp9" alt="I am amit"> </img>
+    <div className="App">
     
     </div>
   );
